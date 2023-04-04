8b96a37000140a80f9d8af2a551fa25052f87a4d
Stuard Naranjo <erlynstuars@gmail.com>
2023-04-04 15:11:42 -0500

    abotu

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,7 +2,7 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: 'Erlyn Naranjo',
   author: 'Tails Azimuth',
   headerTitle: 'TailwindBlog',
   description: 'A blog created with Next.js and Tailwind.css',
