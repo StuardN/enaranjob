4fca887c41e31e5caecb493ab32390488e8fca14
Stuard Naranjo <erlynstuars@gmail.com>
2023-04-05 10:27:55 -0500

    Datos Personales

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -10,14 +10,12 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
-  siteLogo: '/static/images/logo.png',
+  siteLogo: '/static/images/logo1.png',
   image: '/static/images/erlyn.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'address@yoursite.com',
-  github: 'https://github.com',
+  github: 'https://github.com/StuardN',
   twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
   linkedin: 'https://www.linkedin.com',
   locale: 'en-US',
   analytics: {
