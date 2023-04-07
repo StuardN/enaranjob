86d3c0f23dd25d87bb4871800a0363766b674c5f
Stuard Naranjo <erlynstuars@gmail.com>
2023-04-06 22:05:11 -0500

    modificacion

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,22 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Crud en Django',
+    description: `Este CRUD de buses es un proyecto desarrollado en Django, python, html y boostrap que permite al usuario interactuar con los datos de un bus.
+
+    Con este crud, el usuario puede :
+    
+    Crear nuevos registros ingresando información sobre el bus.
+    Leer una lista de todos los buses registrados.
+    Editar los datos existentes.
+    Eliminar registros registrados.`,
+    imgSrc: '/static/images/Crud.png',
+    href: 'https://github.com/StuardN/Crud-Django',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Potfolio',
+    description: `En este portfolio encontrarás más información sobre mí ->`,
+    imgSrc: '/static/images/port.png',
+    href: 'https://stuardn.github.io/Portfolio/',
   },
 ]
 
